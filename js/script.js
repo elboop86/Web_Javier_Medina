a4b957f7330234cc86b2b505cfbf0d29bc379815
Javier Medina <javi86medina@hotmail.com>
2024-03-05 17:21:03 +0100

    arreglo de skills, funciona perfecto

--- js/script.js
+++ js/script.js
@@ -20,17 +20,18 @@ function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300) {
+        // Asigna clases específicas a las barras de progreso de habilidades en un documento HTML, simpre ordenadas
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("htmlcss");
         habilidades[1].classList.add("angular");
         habilidades[2].classList.add("javascript");
         habilidades[3].classList.add("java");
-        habilidades[4].classList.add("comunicacion");
-        habilidades[5].classList.add("trabajo");
-        habilidades[6].classList.add("creatividad");
-        habilidades[7].classList.add("dedicacion");
-        habilidades[8].classList.add("proyect");
-        
+        habilidades[4].classList.add("wordpress");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");   
     } 
 }
 //detecto el scrolling para aplicar la animación de la barra de habilidades
@@ -42,3 +43,13 @@ window.onscroll = function() {
 function limpiarCampos() {
     document.getElementById("myForm").reset();
 }
+
+// Content-reveal
+
+window.addEventListener("scroll", function() {
+  // Detecta si el usuario ha llegado al final de la página
+  if (window.scrollY >= document.body.scrollHeight - document.documentElement.clientHeight) {
+    // Muestra el contenido oculto
+    document.getElementById("content-reveal").style.display = "block";
+  }
+});
